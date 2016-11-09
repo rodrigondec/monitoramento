bdfc85488a06251fb5df97ed0a4968ace40415aa
RodrigoNdeC <rodrigondec@gmail.com>
2016-11-09 14:34:13 -0300

    receptor de dados com atuador

--- ReceptorDeDados/ReceptorDeDados.ino
+++ ReceptorDeDados/ReceptorDeDados.ino
@@ -3,13 +3,13 @@
 #include "RF24.h"
 
 //Armazena os dados recebidos
-int dado, s1, s2, temp;
+int dado, s1, s2,at ,temp, serialData;
 
 //Inicializa a placa nos pinos 9 (CE) e 10 (CS) do Arduino
 RF24 radio(9,10);
 
 //Define o endereco para comunicacao entre os modulos
-const uint64_t pipe = 0xE14BC8F482LL;
+const uint64_t pipe  = 0xE14BC8F482LL;
 
 void setup()
 {
@@ -21,50 +21,85 @@ void setup()
   
   //Entra em modo de recepcao
   radio.openReadingPipe(1,pipe);
-  radio.startListening();
+  radio.openWritingPipe(pipe);
+  radio.startListening();  
 }
 
 void loop()
 {
   dado = 0;
-  
-  while ( ! radio.available() );
-  
-  //Verifica se ha sinal de radio
-  while(radio.available())
-  {
-      radio.read(&dado,sizeof(int));
-  }
 
-  s1 = dado;
-
-  
-  while ( ! radio.available() );
-  
-  //Verifica se ha sinal de radio
-  while(radio.available())
+  if(radio.available())
   {
-      radio.read(&dado,sizeof(int));
-  }
+    //Verifica se ha sinal de radio
+    while(radio.available())
+    {
+        radio.read(&dado,sizeof(int));
+    }
   
-  s2 = dado;
-
-  if(s1 > 500)
-  {
-    s1 -= 1000;
-  }
-  else if(s2 > 500)
-  {
-    s2 -= 1000;
-    temp = s2;
-    s2 = s1;
-    s1 = temp;
-  }
-
+    s1 = dado;
+  
+    while ( ! radio.available() );
     
-  Serial.print(s1);
-  Serial.print(':');
-  Serial.print(s2);
-  Serial.println(":0");
+    //Verifica se ha sinal de radio
+    while(radio.available())
+    {
+        radio.read(&dado,sizeof(int));
+    }
+    
+    s2 = dado;
+
+    while ( ! radio.available() );
+    
+    //Verifica se ha sinal de radio
+    while(radio.available())
+    {
+        radio.read(&dado,sizeof(int));
+    }
+    
+    at = dado;
+  
+    if(s1 > 500)
+    {
+      s1 -= 1000;
+    }
+    else if(s2 > 500)
+    {
+      s2 -= 1000;
+      temp = s2;
+      s2 = s1;
+      s1 = temp;
+    }
+  
+    if(s1 < 0)
+    {
+      s1 = 0;
+    }
+  
+    if(s2 < 0)
+    {
+      s2 = 0;
+    }
+      
+    Serial.print(s1);
+    Serial.print(':');
+    Serial.print(s2);
+    Serial.print(":0:");
+    Serial.println(at);
+  }
+
+  if(Serial.available())
+  {
+    serialData = Serial.parseInt();
+    
+    radio.stopListening();
+    
+    /*Serial.print("Atuador: ");
+    Serial.println(serialData);*/
+    radio.write(&serialData,sizeof(int));
+    delay(100);
+
+    radio.startListening();
+  }
   
 }
